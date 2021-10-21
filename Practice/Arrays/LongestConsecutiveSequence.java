2f3803c86774862c5933205afb3528dc721dbb17
Sagar Gurung <sagarthemaster@gmail.com>
2021-10-21 14:01:41 +0530

    Add files via upload

---
+++ Practice/Arrays/LongestConsecutiveSequence.java
@@ -0,0 +1,31 @@
+public class Solution {
+    // DO NOT MODIFY THE ARGUMENTS WITH "final" PREFIX. IT IS READ ONLY
+    public int longestConsecutive(final int[] A) {
+        
+        HashSet<Integer> hs = new HashSet<Integer>();
+        for(int count = 0; count< A.length;count++){
+            hs.add(A[count]);
+        }
+        int max_length = 0;
+        int length = 0;
+        
+        for(int i: hs){
+            if(!hs.contains(i - 1)){
+                int a = i+1;
+                length = 1;
+                while(hs.contains(a)){
+                    length++;
+                    a++;
+                }
+                
+                if(max_length < length){
+                    max_length = length;
+                }
+            }
+        }
+        
+        
+        
+        return max_length;
+    }
+}
