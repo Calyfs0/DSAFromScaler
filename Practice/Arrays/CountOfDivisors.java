45cb0550c7c28da6810d6e6ee0c3f9581e30aab2
Sagar Gurung <sagarthemaster@gmail.com>
2021-12-07 18:23:26 +0530

    PrimeNumbers

---
+++ Practice/Arrays/CountOfDivisors.java
@@ -0,0 +1,37 @@
+public class Solution {
+    public int[] solve(int[] A) {
+        int max = A[0];
+
+        //Calculate max to initialize divisor array of max length
+        for(int i=1;i<A.length;i++){
+            if(A[i]>max){
+                max = A[i];
+            }
+        }
+
+        int[] divisor_array = new int[max+1];
+        divisor_array[1] = 1;  //Setting number of divisor of 1 as 1
+
+        //Setting initial divisors as 2 as all numbers will have at least 2 divisors i.e. 1 and itself
+        for(int j=2;j<divisor_array.length;j++){
+            divisor_array[j] = 2;
+        }
+
+        //incrementing the number of divisors for each multiples of numbers
+        for(int k = 2;k<divisor_array.length;k++){
+
+            for(int l = k+k;l<divisor_array.length;l+=k){
+                divisor_array[l] += 1;
+            }
+        }
+
+        //Fetching the values of numbers in array directly from divisor_array into the output array
+        for(int c = 0;c<A.length;c++){
+            A[c] = divisor_array[A[c]];
+        }
+
+        return A;
+
+
+    }
+}
--- Practice/Test.js
+++ Practice/Test.js
@@ -1,28 +1,18 @@
-A = 5
-B = 
-[
-  [1, 2, 10],
-  [2, 3, 20],
-  [2, 5, 25]
-]
-let input = []
-let output = []
+int[] arr = new int[N+1];
 
-for(let i=0;i<A;i++){
-    input.push(0)
-    output.push(0)
+for(int i=0;i<N+1;i++){
+    arr[i] = i;
 }
 
-for(let j=0;j<B.length;j++){
-    input[B[0]] = B[2]
-    if(B[1]+1 < B.length){
-        input[B[1]+1] = -B[2]
+for(int j = 2;j*j<N;j++){
+
+    if(arr[j] == j){
+        for(int k = j*j;j<=N;j++){
+            if(arr[k] == k){
+                arr[k] = i;
+            }
+            
+        }
     }
+    
 }
-
-
-for(let k = 1;k < input.length;k++){
-    output[k] = input[k] + output[k-1]
-}
-
-console.log(output) 
