c7e3dc62ad44bd491a014be1c7508651e3988de4
Sagar Gurung <sagarthemaster@gmail.com>
2021-12-08 01:20:40 +0530

    PrimeSubsequences

---
+++ Practice/Arrays/PrimeSubsequences.java
@@ -0,0 +1,25 @@
+public class Solution {
+    public int solve(int[] A) {
+        int count = 0;
+        int mod = 1000000007;
+        int num = 1;
+        for(int i=0;i<A.length;i++){
+            if(isPrime(A[i])){
+                count = count%mod + num%mod;
+                count = count%mod;
+                num = num%mod + num%mod;
+                num = num%mod;
+            } 
+        }
+        return count;
+
+    }
+
+    public boolean isPrime(int N){
+        if(N<2) return false;
+        for(int j = 2;j*j<=N;j++){
+            if(N % j == 0) return false;
+        }
+        return true;
+    }
+}
