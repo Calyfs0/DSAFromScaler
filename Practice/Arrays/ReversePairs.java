d1d6072e28cdba257dacabb6e7dad7e2fe3e290c
Sagar Gurung <sagarthemaster@gmail.com>
2021-12-31 12:26:41 +0530

    31st dec 2021 morning practice

---
+++ Practice/Arrays/ReversePairs.java
@@ -0,0 +1,57 @@
+public class ReversePairs {
+    public int solve(int[] A) {
+        return (int) MergeSort(A, 0, A.length - 1);
+    }
+
+    public long MergeSort(int[] A, int s, int e) {
+
+        if (s == e)
+            return 0;
+
+        long mid = ((long) s + e) / 2;
+        long x = MergeSort(A, s, (int) mid);
+        long y = MergeSort(A, (int) mid + 1, e);
+
+        long z = Merge(A, s, (int) mid + 1, e);
+
+        return (x + y + z);
+    }
+
+    public long Merge(int[] A, int s, int mid, int e) {
+        int p1 = s;
+        int p2 = mid;
+        int p3 = 0;
+        long count = 0;
+        int[] temp = new int[e - s + 1];
+
+        while (p1 < mid && p2 <= e) {
+            if (A[p1] <= A[p2]) {
+                temp[p3++] = A[p1++];
+            } else {
+                temp[p3++] = A[p2++];
+            }
+        }
+
+        while (p1 < mid) {
+            temp[p3++] = A[p1++];
+        }
+        while (p2 <= e) {
+            temp[p3++] = A[p2++];
+        }
+        p1 = s;
+        p2 = mid;
+        while (p1 < mid && p2 <= e) {
+            long val = (long) 2 * A[p2];
+            if (A[p1] > val) {
+                count += mid - p1;
+                p2++;
+            } else {
+                p1++;
+            }
+        }
+        for (int i = 0; i < temp.length; i++) {
+            A[s + i] = temp[i];
+        }
+        return count;
+    }
+}
