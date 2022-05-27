efc249ceae05e313aa9bca940ded588b54260421
Calyfs0 <sagarthemaster@gmail.com>
2022-05-27 22:43:01 +0530

    Bit manipulation Revision done

--- Practice/Bit Manipulation/Java/ReverseBits.java
+++ Practice/Bit Manipulation/Java/ReverseBits.java
@@ -1,19 +1,19 @@
 public class ReverseBits {
-    public long reverse(long a) {
-        int[] bit_array = new int[32];
-
-        for (int i = 0; i < 32; i++) {
-            if ((a & (1 << i)) > 0) {
-                bit_array[i] = 1;
-            }
-        }
-        long output = 0;
-        for (int i = 0; i < bit_array.length; i++) {
-            if (bit_array[i] == 1) {
-                output += ((long) 1 << (31 - i));
-            }
-        }
-
-        return output;
+    public static void main(String[] args) {
+        ReverseBits reverseBits = new ReverseBits();
+        long A = 3;
+        System.out.println(reverseBits.reverse(A));
     }
+    public long reverse(long a) {
+        long reverseInt = 0;
+
+        for(int i=0;i<32;i++){
+            if((a & (1<<i)) > 0){
+                reverseInt |= (long)1<<(31-i);
+            }
+            
+        }
+
+        return reverseInt;
+	}
 }
