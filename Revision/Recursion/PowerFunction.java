77e3892d38329a1128a89fdf7e6f1aac6b6ac76d
Calyfs0 <sagarthemaster@gmail.com>
2022-05-28 22:07:36 +0530

    Three Sum - Two Pointers

---
+++ Revision/Recursion/PowerFunction.java
@@ -0,0 +1,13 @@
+public class PowerFunction{
+    public static void main(String[] args) {
+        PowerFunction powerFunction = new PowerFunction();
+        int A = 2;
+        int B = 3;
+        int C = 3;
+        System.out.println(powerFunction.pow(A, B, C));
+    }
+
+    public int pow(int A, int B, int C){
+
+    }
+}
